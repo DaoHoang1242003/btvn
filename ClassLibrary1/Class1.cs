dd57644d3addf146af1f504b4d029c1b2c145a0f
DaoHoang1242003 <K215480106068@tnut.edu.vn>
2024-09-13 15:42:57 +0700

    thư viện

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public enum San
+    {
+        nhantao,
+        sanco
+    }
+    public class Class1
+    {
+        private const int GiaNhanTAo = 70000;
+        private const int GiaSanCo = 50000;
+
+        public static double TinhTongTien(San san, double Gio)
+        {
+            double giaTien = san == San.nhantao ? GiaNhanTAo : GiaSanCo;
+            return giaTien * Gio;
+        }
+    }
+}
