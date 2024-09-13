31680a2665f2d08f6e380373c8c53b35f5a784ab
DaoHoang1242003 <K215480106068@tnut.edu.vn>
2024-09-13 15:49:25 +0700

    chương trình không dùng thư viện

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Ban muon thue san nao :");
+            Console.WriteLine("\t San nhan tao (70k/h) => Bam phim 1");
+            Console.WriteLine("\t San co (50k/h) => Bam phim 2");
+            Console.Write("\t\t Lua chon cua ban la :"); a = int.Parse(Console.ReadLine());
+            if (a == 1)
+            {
+                a = 70000;
+            }
+            else if (a == 2)
+            {
+                a = 50000;
+            }
+            Console.Write("Ban thue trong bao lau (gio) :"); b = int.Parse(Console.ReadLine());
+            double tongTien = a  * b;
+            Console.WriteLine($"\t Tong tien phai tra la: {tongTien} VND");
+            Console.ReadLine();
+        }
+    }
+}
